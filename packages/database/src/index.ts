1074be841b65564f40b4b6cb3b798fa9d8fd92a4
João Pedro Tavares <34795195+JohnPitter@users.noreply.github.com>
2026-03-08 20:02:01 -0300

    chore(luxview): Update database client to use PostgreSQL with environment variables

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,2 +1,11 @@
-export { db, client } from "./connection";
-export * as schema from "./schema/index";
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schema from './schema';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema });
+
+export * from './schema';
